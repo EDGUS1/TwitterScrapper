b5a58bf86027de245f7dac91091267878ca6c947
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-04-12 23:24:58 -0500

    feat: Add CrossOrigin

--- src/app.py
+++ src/app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, Request, Form
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from scrapper import consultar
 
 app = FastAPI()
+
+origins = [
+    "https://railway.app/",
+    "https://twitterscrapper-production.up.railway.app/",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="src\\templates")
 
